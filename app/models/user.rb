d97a112df2739ed55255081c71149423489e7d4b
berkut <berkutakarapper@gmail.com>
2021-01-06 02:24:01 +0300

    Added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
-  validates :email, format: { with: /\w+@\w+(?:-\w+)?\.[A-Za-z]{2,4}/i }
+  validates :email, format: { with: /\A\w+@\w+(?:-\w+)?\.[A-Za-z]{2,4}+\z/i }
   validates :username, length: { maximum: 40 }
   validates :username, format: { with: /\A[a-zA-Z_]+\z/ }
 
