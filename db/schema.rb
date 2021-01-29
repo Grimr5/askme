c2064e7502a386e50d3f47133f6e35bcea1d8bdd
berkut <berkutakarapper@gmail.com>
2021-01-30 00:22:48 +0300

    AddIndex migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_215215) do
+ActiveRecord::Schema.define(version: 2021_01_29_211220) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_01_03_215215) do
     t.string "password_hash"
     t.string "password_salt"
     t.string "avatar_url"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "questions", "users"
